f062b8881dc8dc6b20d41da7efd65459f922ec0a
Katie Corcoran <corcorankt1@gmail.com>
2016-04-07 19:10:58 -0400

    add changing class to seats

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 
 $(document).ready(function(){
-$( "#thanks" ).dialog({ autoOpen: false });
-$( "#submit-button" ).click(function() {
-$( "#thanks" ).dialog( "open" );
-});
+	
+	$( "#thanks" ).dialog({ autoOpen: false });
+	$( "#submit-button" ).click(function() {
+	$( "#thanks" ).dialog( "open" );
+	});
+
+	$( "div.seats" ).mouseenter(function() {
+  	$( this ).fadeTo("fast", .10) 
+	});
+
+	$("div.seats").mouseout(function() {
+  	$( this ).fadeTo("fast", 1) 
+  	});
+
+	$("div.seats").click(function () {
+	$(this).css('background', 'grey').fadeTo("fast", 1).text("selected") ;
+	});
+
 });
