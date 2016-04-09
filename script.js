ae63fdfad296b5a2533ca57a0d90a7879b5b242a
Katie Corcoran <corcorankt1@gmail.com>
2016-04-09 10:23:08 -0400

    update toggle on seats

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
 	
-	var counter = 0;
+	// var counter = 0;
 
-	$( "#thanks" ).dialog({ autoOpen: false });
-	$( "#submit-button" ).click(function() {
-	$( "#thanks" ).dialog( "open" );
-	});
+	// $( "#thanks" ).dialog({ autoOpen: false });
+	// $( "#submit-button" ).click(function() {
+	// $( "#thanks" ).dialog( "open" );
+	// });
 
 	$( "div.seats" ).mouseenter(function() {
   	$( this ).fadeTo("fast", .10) 
 	});
 
 	$("div.seats").mouseout(function() {
-  	$( this ).fadeTo("fast", 1) 
+  	$(this).fadeTo("fast", 1) 
   	});
 
-	$("div.seats").click(function () {
-	var seatNumber = $("p.seat").val();
-	$("#reservation").append(seatNumber);
-	$(this).css('background', 'grey').fadeTo("fast", 1).text("selected") ;
-     counter ++;
-
+	$("p.seat").on("click", function(){
+		$(this).toggleClass("selected");
 	});
+	// var seatNumber = $("p.seat").val();
+	// $("#reservation").append(seatNumber);
+	// $(this).css('background', 'grey').fadeTo("fast", 1).text("selected") ;
+ //     counter ++;
+
+	// });
 
     $(".button").click(function(){
 
